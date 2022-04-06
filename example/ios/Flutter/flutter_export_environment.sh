94478d6774c65e11889a804e9358db0790e021b8
inRush <inrush@inrush.cn>
2022-04-06 22:30:36 +0800

    [Update] upgrade flutter version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Workspace\WorkSpace\Flutter\sdk"
-export "FLUTTER_APPLICATION_PATH=D:\Workspace\WorkSpace\Flutter\projects\TipDialog\example"
+export "FLUTTER_ROOT=D:\WorkSpace\Flutter\sdk"
+export "FLUTTER_APPLICATION_PATH=D:\WorkSpace\Flutter\projects\TipDialog\example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\Workspace\WorkSpace\Flutter\sdk\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
